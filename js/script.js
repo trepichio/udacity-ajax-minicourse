afa2b7f7e945186f936f1feb0e9106efad9ac228
João Trepichio <trepichio@gmail.com>
2015-04-12 06:03:16 -0300

    Added NYT Articles search and GoogleStreet View backg-image
    -request a google street view image from location provided on the form
    -Send values from form inputs to NYT API to request some articles
    -Appended the results to nyt-articles list
    -Styled the list

--- js/script.js
+++ js/script.js
@@ -6,14 +6,84 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $img = $('.bgimg');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    $img.remove();
 
     // load streetview
+    var streetviewURL = "http://maps.googleapis.com/maps/api/streetview?";
 
-    // YOUR CODE GOES HERE!
+    var $street = $("#street");
+    var $city = $("#city");
+    var address = $street.val() + ', ' + $city.val();
+
+    $greeting.text('So, you want to live at ' + address + '?');
+    var p = {
+        "location": address,
+        "size": "640x480",
+        "sensor": false
+    };
+
+    var  bgImg = streetviewURL;
+
+    for (var key in p) {
+        bgImg += key + "=" + p[key] + "&";
+    }
+
+    $body.append('<img class="bgimg" src="'+ bgImg+'">');
+
+    //load New York Times Article API
+    var response_format = ".json";
+    var nytart_fl = [
+            "headline",
+            "snippet",
+            "keywords",
+            "web_url"
+    ];
+
+    var nyt_source = "The New York Times";
+    var api_key = "a064a541e3631cfee2e539a7c693bf7b:5:71831437";
+    var nyt_artURL = "http://api.nytimes.com/svc/search/v2/articlesearch";
+    var nyt_artSearch = [
+        "q=" + $street.val(),
+        "fq=" + [   'source:("' + nyt_source + '")',
+                    'glocations:("' + $city.val() + '")'
+                ].join(" AND "),
+        "fl=" + nytart_fl.join(",")
+    ];
+
+    var nyt_artrequest = nyt_artURL + response_format + 
+        "?" + nyt_artSearch.join("&") + "&api-key=" + api_key;
+        console.log(nyt_artrequest);
+
+    $.getJSON(nyt_artrequest, function(data){
+        console.log(data);
+
+        var items = [];
+        var docs = data.response.docs;
+        $.each( docs, function( key, val ) {
+            var k = $.map(val.keywords, function (obj) {
+                            return obj.value;
+                        }).join(" ");
+            
+            items.push( "<dt class='article' id='headline_" + key + "'>" + val.headline.main + "</dt>"+ 
+                        "<dd id='dt_"+ key + "'>"+ val.snippet + "<br/>" + 
+                        "<a id='link_" + key + "' href='" + val.web_url + "'>Learn More </a> <br/>" + 
+                        "<span id='keywords_" + key + "' class='keywords'>" + k + 
+                        "</span>" + 
+                        "</dd>"
+            );
+        });
+        
+        $(items.join( "" )).appendTo( $nytElem );
+
+
+    });
+
+
 
     return false;
 };
